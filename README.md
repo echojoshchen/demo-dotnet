9bf6974b94f9234bc9d74abc4e77c4f52ac778b2
jlchen <echojoshchen@gmail.com>
2023-12-16 11:31:13 -0500

    Add HTTP route

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Demo of .NET service
 ## Resources
 
 - [Generated assets](https://learn.microsoft.com/en-us/aspnet/core/grpc/basics?view=aspnetcore-7.0#generated-c-assets)
+- [gRPC to JSON transcoding](https://learn.microsoft.com/en-us/aspnet/core/grpc/json-transcoding)
--- Services/DemoService.cs
+++ Services/DemoService.cs
@@ -12,14 +12,14 @@ public class DemoService : DemoApi.DemoApiBase
         _logger = logger;
     }
 
-    public override Task<DemoContainer> DoSomething(DemoContainer request, ServerCallContext context)
+    public override Task<DemoContainer> DoSomething(DemoRequest request, ServerCallContext context)
     {
         DemoObject obj = new DemoObject {
             Name = "obj name",
         };
         DemoContainer resp = new DemoContainer
         {
-            Id = "My ID",
+            Id = request.Id,
         };
         resp.Objects.Add(obj);
 
