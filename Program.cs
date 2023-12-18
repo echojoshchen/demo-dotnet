a0b5a264c94d2b57211b8e675cac484861095e76
jlchen <echojoshchen@gmail.com>
2023-12-17 23:12:46 -0500

    Add latency log

--- Program.cs
+++ Program.cs
@@ -5,6 +5,10 @@
 /// </summary>
 
 using Demo.Search;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Diagnostics;
+using System.Threading.Tasks;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +18,19 @@ builder.Services.AddGrpc().AddJsonTranscoding();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.Use(async (context, next) =>
+{
+    var stopwatch = Stopwatch.StartNew();
+
+    await next();
+
+    stopwatch.Stop();
+    var elapsedMilliseconds = stopwatch.ElapsedMilliseconds;
+
+    // Report the latency metric
+    Console.WriteLine($"Request latency: {elapsedMilliseconds}ms");
+});
+
 app.MapGrpcService<QueryService>();
 app.MapGrpcService<IndexService>();
 app.MapGet("/", () => "Demo search app with gRPC and SQLite.");
